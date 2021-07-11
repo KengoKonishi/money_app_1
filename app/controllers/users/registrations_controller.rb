aa3ceca1130db0747a33e2ae25bff2c59f5e44a5
KengoKonishi <konitanz627@gmail.com>
2021-07-11 14:37:41 +0000

    login_func 07112337

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,7 +11,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-     super
+    super
   end
 
   # GET /resource/edit
@@ -21,7 +21,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   def update
-     super
+    super
   end
 
   # DELETE /resource
@@ -42,23 +42,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-     devise_parameter_sanitizer.permit(
-       :sign_up, keys: [:username, :email, :password, :password_confirmation ])
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:username, :email, :password, :password_confirmation ])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-     devise_parameter_sanitizer.permit(
-       :account_update, keys: [:username, :email, :password, :password_confirmation ])
+    devise_parameter_sanitizer.permit(
+      :account_update, keys: [:username, :email, :password, :password_confirmation ])
   end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-     super(resource)
+    super(resource)
   end
 
   # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
-     super(resource)
+    super(resource)
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,7 +10,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
-     super
+    super
   end
 
   # DELETE /resource/sign_out
@@ -19,18 +19,18 @@ class Users::SessionsController < Devise::SessionsController
   # end
   
   def after_sign_in_path_for(user)
-     mypage_path(user)
+    mypage_path(user)
   end
  
   def after_sign_out_path_for(user)
-     login_path(user)
+    login_path(user)
   end
 
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-     devise_parameter_sanitizer.permit(
-       :sign_in, keys: [:username, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(
+      :sign_in, keys: [:username, :email, :password, :password_confirmation])
   end
 end
