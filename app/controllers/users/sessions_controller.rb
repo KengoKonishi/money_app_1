484aa1b6cab33c96bc1407eb81a0cb1cedb63689
KengoKonishi <konitanz627@gmail.com>
2021-07-23 07:26:58 +0000

    createtable0723

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -23,7 +23,7 @@ class Users::SessionsController < Devise::SessionsController
   end
  
   def after_sign_out_path_for(resource)
-    home_top_path
+    root_path
   end
 
   protected
---
+++ test/models/basic_expense_category_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BasicExpenseCategoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
